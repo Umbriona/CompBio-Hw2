ad4904504c98d23eb9bb26edd1f8a1c65c64ed40
Umbriona <33811221+Umbriona@users.noreply.github.com>
2018-04-28 11:50:36 +0200

    new modules

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-import numpy as py
+import numpy as np
+import Stirling as stir
+
+def frange(start, stop, step):
+    i = start
+    while i < stop:
+        yield i
+        i += step
+n=100
+m=frange(1,100,1)
+mutationProbability=0.1
+
+
+print(stir.stirling1(100,1))
+
 
