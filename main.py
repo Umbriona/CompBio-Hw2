61c1e4f2efb095ce28aa3e5339e828484f2b9a2d
Umbriona <sandra.viknander@gmail.com>
2018-04-28 13:11:56 +0200

    First plot

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import numpy as np
 import Stirling as stir
+import matplotlib.pyplot as plt
+from decimal import Decimal
 
 def frange(start, stop, step):
     i = start
     while i < stop:
         yield i
         i += step
+def sumProb(mutation,n):
+    i=1
+    val=0
+    while i<n:
+        val = val+(i+mutation)
+        i+=1
+    return val
+dt = np.dtype(np.int64)
 n=100
 m=frange(1,100,1)
 mutationProbability=0.1
+stirlingNumber = np.zeros([n,])
+mNumbers = np.zeros([n,])
+i = 0
+for num in stirlingNumber:
+    stirlingNumber[i] = Decimal(stir.stirling1(100,i+1))
+    i+=1
+i=0
+for num in stirlingNumber:
+    mNumbers[i] = i+1
+    i+=1
 
+ewensSampling = np.array(stirlingNumber*mutationProbability**(mNumbers-1)/sumProb(mutationProbability,n))
 
-print(stir.stirling1(100,1))
-
-
+plt.plot(mNumbers,ewensSampling)
